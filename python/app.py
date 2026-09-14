3cb1f3b34ffceabf1a41ee049f158fd9c08afccd
langhakka <langhakka9527@gmail.com>
2026-09-14 08:36:46 +0800

    Update app.py

--- python/app.py
+++ python/app.py
@@ -23,8 +23,8 @@ config = {
     'NEZHA_SERVER': os.environ.get('NEZHA_SERVER', ''), # 哪吒面板地址，v1格式: nezha.xxx.com:8008  v0格式： nezha.xxx.com
     'NEZHA_PORT': os.environ.get('NEZHA_PORT', ''),     # 哪吒v1请留空，哪吒v0 agent端口
     'NEZHA_KEY': os.environ.get('NEZHA_KEY', ''),       # 哪吒v1的NZ_CLIENT_SECRET或哪吒v0-agent密钥
-    'ARGO_DOMAIN': os.environ.get('ARGO_DOMAIN', ''),   # 固定隧道域名,留空即启用临时隧道
-    'ARGO_AUTH': os.environ.get('ARGO_AUTH', ''),       # 固定隧道token或json,留空即启用临时隧道,json获取:https://json.zone.id
+    'ARGO_DOMAIN': os.environ.get('ARGO_DOMAIN', 'monkey.adg007.kdns.fr'),   # 固定隧道域名,留空即启用临时隧道
+    'ARGO_AUTH': os.environ.get('ARGO_AUTH', 'eyJhIjoiYjllZmFhZDk2NmQyOTUwMDk5YzVjYjcyOTk5NGRkOWYiLCJ0IjoiYWEzNmE1MDktODM4NC00NTIwLTliNzktZjBlYmNlNTZlYjI3IiwicyI6Ik56RmtOV1V3WWpNdFpqSmtPUzAwWlRNMkxXRmtNRGt0TXpkbE5XRXpPR1V5TnpObCJ9'),       # 固定隧道token或json,留空即启用临时隧道,json获取:https://json.zone.id
     'ARGO_PORT': os.environ.get('ARGO_PORT', '8001'),   # argo端口 使用固定隧道token,cloudflare后台设置的端口需和这里对应
     'CFIP': os.environ.get('CFIP', 'saas.sin.fan'),     # 优选域名或优选ip
     'CFPORT': os.environ.get('CFPORT', '443'),          # 优选域名或优选ip对应端口
